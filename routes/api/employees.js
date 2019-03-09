541b961ed58ae262f5f243282f244dc1cb26285d
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-08 21:16:47 -0800

    Setup current route

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -85,7 +85,7 @@ router.post('/login', (req, res) => {
           if (isMatch) {
             const payload = { id: employee.id, name: employee.fname };
 
-            jwt.sign(payload, keys.secretOrKeys, { expiresIn: 3600 }, (err, token) => {
+            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
               res.json({
                 success: true,
                 token: "Bearer " + token
