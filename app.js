db7121f746bb164de79d0cdfcea7d4b02d14798d
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-08 19:16:07 -0800

    Setup employee models and routes

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const employees = require("./routes/api/employees");
+const bodyParser = require('body-parser');
 
 mongoose.connect(db, {useNewUrlParser: true})
   .then(() => console.log("connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
-app.get("/", (req, res) => res.send("Hello Test"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/employees", employees);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ models/Employee.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  fname: {
+    type: String,
+    required: true
+  },
+  lname: {
+    type: String,
+    required: true
+  },
+  e_id: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Employee = mongoose.model('employees', EmployeeSchema);
