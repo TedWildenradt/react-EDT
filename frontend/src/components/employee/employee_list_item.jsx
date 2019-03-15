3f7cb3c46c2f5eb745328dc88807090f71700453
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-14 20:06:04 -0700

    Optimize employee list items

--- frontend/src/components/employee/employee_list_item.jsx
+++ frontend/src/components/employee/employee_list_item.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-function EmployeeListItem({employee}) {
-  return(
-    <div className="EmployeeListItem">
-      <h1>{employee.fname} {employee.lname} - {employee.e_id}</h1>
-    </div>
-  )
+class EmployeeListItem extends React.Component {
+  shouldComponentUpdate(nextProps){
+    if (this.props === nextProps){
+      return false;
+    }
+    return true;
+  }
+
+  render(){
+    const {employee} = this.props;
+    return(
+      <div className="EmployeeListItem">
+        <h1>{employee.fname} {employee.lname} - {employee.e_id}</h1>
+      </div>
+    )
+
+  }
 }
 
 export default EmployeeListItem;
